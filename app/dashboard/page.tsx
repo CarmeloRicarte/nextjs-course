d288a22d27bad8134616a919b8ac1e0e1edc5430
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-02-07 16:39:24 +0100

    fetch data from Rick and Morty API for show an image of Morty

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,17 @@
-function Dashboard() {
-  return <div>Dashboard</div>;
+import Image from "next/image";
+
+async function getData() {
+  const response = await fetch("https://rickandmortyapi.com/api/character/2");
+  return response.json();
+}
+
+async function Dashboard() {
+  const data = await getData();
+  return (
+    <div>
+      <Image width="100" height="100" src={data.image} alt="Morty" />
+    </div>
+  );
 }
 
 export default Dashboard;
