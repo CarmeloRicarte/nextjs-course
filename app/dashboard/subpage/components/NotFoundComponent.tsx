f604e37c0e5fc59054f008103062c9c010156fb4
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-03-14 16:11:49 +0100

    feat: delete initial files before init with  rick and morty project

--- app/dashboard/subpage/components/NotFoundComponent.tsx
+++
@@ -1,8 +0,0 @@
-import { notFound } from "next/navigation";
-
-function NotFoundComponent() {
-  notFound(); // this will render the not-found.tsx page
-  return <>Esto no se va a ver</>;
-}
-
-export default NotFoundComponent;
--- app/dashboard/subpage/not-found.tsx
+++
@@ -1,6 +0,0 @@
-// use for 404 error handling
-function NotFound() {
-  return "La página no existe";
-}
-
-export default NotFound;
