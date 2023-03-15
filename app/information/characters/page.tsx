dee324663ed74f2b71316edaf85a32abe39d53d2
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-03-15 10:13:59 +0100

    feat: list characters

---
+++ app/information/characters/page.tsx
@@ -0,0 +1,21 @@
+import { Card, Navigator } from '../../../components';
+import { Routes } from '../../../models';
+import { getCharacters } from './services/characters.service';
+
+async function fetchCharacters() {
+  return getCharacters();
+}
+
+async function Characters() {
+  const characters = await fetchCharacters();
+  return (
+    <>
+      <Navigator pathNames={[Routes.HOME, Routes.LOCATIONS]} />
+      {characters.map((character) => (
+        <Card key={character.id} data={character} />
+      ))}
+    </>
+  );
+}
+
+export default Characters;
