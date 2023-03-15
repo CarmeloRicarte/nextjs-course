c4f0f1e033a155bdc83c0fd2334d20dba28abf1b
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-03-15 10:09:36 +0100

    feat: create card component

---
+++ components/Card/Card.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+
+import styles from './Card.module.scss';
+
+interface CardData {
+  name: string;
+  created: string;
+  type?: string;
+  image?: string;
+}
+
+interface Props {
+  data: CardData;
+}
+
+function Card({ data }: Props) {
+  let formattedType = data.type;
+  formattedType ||= 'No Type';
+  return (
+    <div className={styles.Card}>
+      <p>Name: {data.name}</p>
+      <p>Type: {formattedType}</p>
+      <p>Created: {data.created}</p>
+      {!!data.image && (
+        <Image
+          width='100'
+          height='100'
+          src={data.image}
+          alt={data.name}
+          priority
+        />
+      )}
+    </div>
+  );
+}
+
+export default Card;
