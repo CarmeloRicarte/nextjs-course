f57410ddde5d09252db5eb64396b897fdd5fd91f
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-03-20 16:56:22 +0100

    feat: add Rototo font from Google Fonts and Fira Code font from local with next/font package

--- app/information/layout.tsx
+++ app/information/layout.tsx
@@ -1,9 +1,14 @@
 import { ReactNode } from 'react';
 
+import { firacode } from '../models';
 import styles from './information-layout.module.scss';
 
 function InformationLayout({ children }: { children: ReactNode }) {
-  return <div className={styles.informationLayout}>{children}</div>;
+  return (
+    <div className={`${firacode.className} ${styles.informationLayout}`}>
+      {children}
+    </div>
+  );
 }
 
 export default InformationLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import './tailwind-global.scss';
 
+import { Roboto } from '@next/font/google';
+
 import StyledComponentsRegistry from './lib/registry';
+
+// all app will have Roboto font
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['italic', 'normal'],
+  subsets: ['latin'],
+  variable: '--font-roboto',
+  display: 'optional',
+});
 
 export default function RootLayout({
   children,
@@ -9,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <head />
+      <head className={roboto.className} />
       <body>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
