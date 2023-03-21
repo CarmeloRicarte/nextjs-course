2d40ce1fb62d9a4bdf9cf18caf85c23fe1736f96
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-03-21 16:49:01 +0100

    feat: add special config for next middlewares

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  skipTrailingSlashRedirect: true, // skip trailing slash
+  skipMiddlewareUrlNormalize: true, // normalize URL, because by default, NextJS routes starts with '/_next/data/build-id/*name of route.json
   experimental: {
     appDir: true,
   },
   images: {
     remotePatterns: [
       {
-        hostname: "rickandmortyapi.com",
+        hostname: 'rickandmortyapi.com',
       },
     ],
   },
