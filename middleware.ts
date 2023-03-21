9f19b50b1bd500fc2d0271a40775598a0bc2c0c0
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-03-21 16:39:21 +0100

    feat: create a middleware, similar to an Angular Guard

---
+++ middleware.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set('Content-Type', 'application/json');
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+
+  if (request.nextUrl.pathname.endsWith('/information')) {
+    return NextResponse.redirect(
+      new URL('/information/characters', request.url)
+    );
+  }
+
+  if (request.nextUrl.pathname.startsWith('/information')) {
+    const user = {
+      name: 'John Doe',
+      authenticated: false,
+    };
+    if (!user.authenticated) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+  }
+
+  return response;
+}
+
+// the middleware will act in the routes that matches with routes included in the matcher
+export const config = {
+  matcher: [
+    '/information/:path*',
+    '/((?!api|_next/static|_next/image|favicon.ico).*)', // don't act in static content
+  ],
+};
